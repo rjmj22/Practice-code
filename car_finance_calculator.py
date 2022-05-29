f008344fdfedceb5aa09407681b7bd494d886486
Ronnie Jones <rjmj22@gmail.com>
2022-05-29 11:19:39 -0400

    trade in and down payment and credit score values added

--- car_finance_calculator.py
+++ car_finance_calculator.py
@@ -19,3 +19,38 @@ subprime = df.loc[7, :].values.tolist()
 
 deep_subprime = df.loc[9, :].values.tolist()
 
+
+def main():
+    print("This is a Auto loan finance calculator. Please enter whole numbers only, no commas needed!")
+    
+    # trade in and down payment values
+    while True:
+        try:
+            trade_in = float(input("If you have a trade in enter value, if not put 0: "))
+        except ValueError:
+            print('Numerics only')
+            continue
+        else:
+            break
+    
+    while True:
+        try:
+            down_payment = float(input("If you have a down payment enter value, if not put 0: "))
+        except ValueError:
+            print('Numerics only')
+            continue
+        else:
+            break
+            
+    while True:
+        try:
+            credit_score = float(input("Enter credit score number between 300-850: "))
+            if (credit_score < 300) or (credit_score > 850):
+                print("Invalid number: try again")
+                continue
+        except ValueError:
+            print('Numerics only')
+            continue
+        else:
+            break
+main()
