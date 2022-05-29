2ca3e2156715a2e40d31bf3ab3d29e4b79cbd76a
Ronnie Jones <rjmj22@gmail.com>
2022-05-29 10:36:17 -0400

    Getting csv date to lists

--- car_finance_calculator.py
+++ car_finance_calculator.py
@@ -1,7 +1,21 @@
+# Importing file data.csv
+# Make sure to update the path as needed
+
 import pandas as pd
 
 df = pd.read_csv('D:\\code\data.csv')
 
 df = df.dropna()
 
-print(df)
+# Getting csv data to lists
+
+superprime = df.loc[1, :].values.tolist()
+
+prime = df.loc[3, :].values.tolist()
+
+nonprime = df.loc[5, :].values.tolist()
+
+subprime = df.loc[7, :].values.tolist()
+
+deep_subprime = df.loc[9, :].values.tolist()
+
