8e5a53510ec6540ddc186059aa0cb9382313e1d4
Ronnie Jones <rjmj22@gmail.com>
2022-07-23 10:55:09 -0400

    ex26 upload

---
+++ ex 26.py
@@ -0,0 +1,6 @@
+# Write a Python program to create a histogram from a given list of integers
+from matplotlib import pyplot as plt
+numbers = [150, 40, 220, 10, 300]
+plt.hist(numbers)
+
+plt.show()
