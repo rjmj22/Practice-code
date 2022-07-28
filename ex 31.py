374800c2c7cb77ded818a02092fe227bf6318271
Ronnie Jones <rjmj22@gmail.com>
2022-07-28 19:17:45 -0400

    ex31 upload

---
+++ ex 31.py
@@ -0,0 +1,5 @@
+# Write a Python program to compute the greatest common divisor (GCD)
+from math import gcd
+num_one = int(input("Enter number: "))
+num_two = int(input("Enter number: "))
+print(gcd(num_one, num_two))
