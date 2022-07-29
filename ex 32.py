3f357c585b59fc5e316f7c6e536006715acbd881
Ronnie Jones <rjmj22@gmail.com>
2022-07-29 18:55:58 -0400

    ex32 upload

---
+++ ex 32.py
@@ -0,0 +1,5 @@
+# Write a Python program to get the least common multiple (LCM) of two positive integers
+from math import lcm
+num_one = int(input("Enter number: "))
+num_two = int(input("Enter number: "))
+print(lcm(num_one, num_two))
